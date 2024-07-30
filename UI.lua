7785b464c467a317369fd17c514c086760859eb3
Tom <130856593+PossiblyTom@users.noreply.github.com>
2024-07-30 22:10:09 +0200

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -1,10 +1,10 @@
 local a=game:GetService"UserInputService"
 local b=game:GetService"CoreGui"
-
+local c=game:GetService"TweenService"
 
 local function checkIfUIExists()
-for c,d in pairs(b:GetChildren())do
-if d:IsA"ScreenGui"and d.Name=="TomHubUI"then
+for d,e in pairs(b:GetChildren())do
+if e:IsA"ScreenGui"and e.Name=="TomHubUI"then
 print"stop"
 return true
 end
@@ -12,201 +12,187 @@ end
 return false
 end
 
-
 local function createUI()
 if checkIfUIExists()then
 return
 end
 
-local c=Instance.new"ScreenGui"
-c.Name="TomHubUI"
-local d=Instance.new"Frame"
-local e=Instance.new"TextLabel"
-local f=Instance.new"Frame"
-local g=Instance.new"TextButton"
-local h=Instance.new"TextButton"
-local i=Instance.new"TextButton"
-local j=Instance.new"TextButton"
-local k=Instance.new"Frame"
-local l=Instance.new"Frame"
-local m=Instance.new"Frame"
-local n=Instance.new"Frame"
+local d=Instance.new"ScreenGui"
+d.Name="TomHubUI"
 
+local e=Instance.new"Frame"
+local f=Instance.new"TextLabel"
+local g=Instance.new"Frame"
+local h=Instance.new"Frame"
 
-c.Parent=b
+local function addUICorner(i,j)
+local k=Instance.new"UICorner"
+k.CornerRadius=UDim.new(0,j)
+k.Parent=i
+end
 
-
-d.Parent=c
-d.BackgroundColor3=Color3.fromRGB(40,40,40)
-d.Size=UDim2.new(0,450,0,600)
-d.Position=UDim2.new(0.5,-225,0.5,-300)
-d.Active=true
-d.Draggable=true
-
+d.Parent=b
 
 e.Parent=d
-e.Text="Tom's Hub | Who needs sex anyways?"
-e.Font=Enum.Font.SourceSans
-e.TextSize=24
-e.TextColor3=Color3.fromRGB(255,255,255)
-e.BackgroundColor3=Color3.fromRGB(30,30,30)
-e.BackgroundTransparency=0
-e.Position=UDim2.new(0,0,0,0)
-e.Size=UDim2.new(1,0,0,50)
-e.BorderSizePixel=0
+e.BackgroundColor3=Color3.fromRGB(40,40,40)
+e.Size=UDim2.new(0,450,0,600)
+e.Position=UDim2.new(0.5,-225,0.5,-300)
+e.Active=true
+e.Draggable=true
+addUICorner(e,15)
 
-
-f.Parent=d
+f.Parent=e
+f.Text="Tom's Hub | Who needs sex anyways?"
+f.Font=Enum.Font.SourceSans
+f.TextSize=24
+f.TextColor3=Color3.fromRGB(255,255,255)
 f.BackgroundColor3=Color3.fromRGB(30,30,30)
-f.Size=UDim2.new(1,0,0,30)
-f.Position=UDim2.new(0,0,0,45)
+f.Size=UDim2.new(1,0,0,50)
 f.BorderSizePixel=0
 
 
-local o=Instance.new"Frame"
-o.Parent=d
-o.BackgroundColor3=Color3.fromRGB(50,50,50)
-o.Size=UDim2.new(1,0,1,-80)
-o.Position=UDim2.new(0,0,0,80)
-o.Visible=true
-o.BorderSizePixel=0
+g.Parent=e
+g.BackgroundColor3=Color3.fromRGB(30,30,30)
+g.Size=UDim2.new(1,0,0,30)
+g.Position=UDim2.new(0,0,0,45)
+g.BorderSizePixel=0
 
 
-local function createTab(p,q,r,s,t)
-p.Parent=f
-p.Size=UDim2.new(0,80,0,30)
-p.Position=q
-p.BackgroundColor3=Color3.fromRGB(60,60,60)
-p.TextColor3=Color3.fromRGB(255,255,255)
-p.Font=Enum.Font.SourceSans
-p.TextSize=18
-p.Text=r
-p.BorderSizePixel=0
+h.Parent=e
+h.BackgroundColor3=Color3.fromRGB(50,50,50)
+h.Size=UDim2.new(1,0,1,-80)
+h.Position=UDim2.new(0,0,0,80)
+h.Visible=true
+h.BorderSizePixel=0
+addUICorner(h,15)
 
+local function createTab(i,j,k,l,m)
+i.Parent=g
+i.Size=UDim2.new(0,80,0,30)
+i.Position=j
+i.BackgroundColor3=Color3.fromRGB(60,60,60)
+i.TextColor3=Color3.fromRGB(255,255,255)
+i.Font=Enum.Font.SourceSans
+i.TextSize=18
+i.Text=k
+i.BorderSizePixel=0
+addUICorner(i,10)
 
-s.Parent=o
-s.BackgroundColor3=Color3.fromRGB(50,50,50)
-s.Size=UDim2.new(1,0,1,0)
-s.Position=UDim2.new(0,0,0,0)
-s.Visible=false
-s.Name=r
+l.Parent=h
+l.BackgroundColor3=Color3.fromRGB(50,50,50)
+l.Size=UDim2.new(1,0,1,0)
+l.Position=UDim2.new(0,0,0,0)
+l.Visible=false
+l.Name=k
+addUICorner(l,15)
 
+local n=(l.AbsoluteSize.Y-#m*60)/2
 
-local u=0
-local v=10
-for w,x in ipairs(t)do
-u=u+50+v
-end
-u=u-v
+for o,p in ipairs(m)do
+local q=Instance.new"TextButton"
+q.Parent=l
+q.Size=UDim2.new(0,177,0,50)
+q.Position=UDim2.new(0.5,-88.5,0,n+(o-1)*60)
+q.BackgroundColor3=Color3.fromRGB(70,70,70)
+q.TextColor3=Color3.fromRGB(255,255,255)
+q.Font=Enum.Font.SourceSans
+q.TextSize=20
+q.Text=p.text
+q.BorderSizePixel=0
+addUICorner(q,10)
 
-
-local y=(s.AbsoluteSize.Y-u)/2
-
-
-for z,A in ipairs(t)do
-local B=Instance.new"TextButton"
-B.Parent=s
-B.Size=UDim2.new(0,177,0,50)
-B.Position=UDim2.new(0.5,-88.5,0,y+(z-1)*(50+v))
-B.BackgroundColor3=Color3.fromRGB(70,70,70)
-B.TextColor3=Color3.fromRGB(255,255,255)
-B.Font=Enum.Font.SourceSans
-B.TextSize=20
-B.Text=A.text
-B.BorderSizePixel=0
-
-
-B.MouseButton1Click:Connect(function()
-loadstring(A.loadStringCode)()
+q.MouseButton1Click:Connect(function()
+loadstring(p.loadStringCode)()
 end)
 end
 end
 
-
-local p={
+local i={
 {text="Aimbot + ESP",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-MainLoader"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-MainLoader"))()
         ]]},
 {text="Infinite Ammo",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-InfAmmo"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-InfAmmo"))()
         ]]},
-{text="Fire Rate",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-FireRateTesting"))()
+{text="Fire Rate (Very Blatant)",loadStringCode=[[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-FireRateTesting"))()
         ]]},
 {text="No Equip Time",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/EquipTimeChanger"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/EquipTimeChanger"))()
         ]]},
-{text="No Spread",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-NoSpread"))()
+{text="No Spread (Blatant)",loadStringCode=[[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-NoSpread"))()
         ]]},
-{text="Auto All",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-FullAutoEverything"))()
+{text="Auto All (Blatant)",loadStringCode=[[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-FullAutoEverything"))()
         ]]},
 {text="No Recoil",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-NoRecoil"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Arsenal-NoRecoil"))()
         ]]}
 }
 
-local q={
+local j={
 {text="Aimbot + ESP",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Rivals-MainLoader"))()
-        ]]},
-}
-
-local r={
-{text="ESP",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/PhantomForces-BoxESP"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/Rivals-MainLoader"))()
         ]]}
 }
 
-local s={
+local k={
+{text="ESP",loadStringCode=[[
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/PhantomForces-BoxESP"))()
+        ]]}
+}
+
+local l={
 {text="Fly",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/arsenalfly"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/flying"))()
         ]]},
 {text="Aimbot (NO ESP)",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/gurraim"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/gurraim"))()
         ]]},
 {text="Noclip",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/noclip"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/noclip"))()
         ]]},
 {text="Fullbright",loadStringCode=[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/fullbright"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/TomsStuff/Toms-Executor/main/fullbright"))()
         ]]}
 }
 
+local m=Instance.new"TextButton"
+local n=Instance.new"TextButton"
+local o=Instance.new"TextButton"
+local p=Instance.new"TextButton"
+local q=Instance.new"Frame"
+local r=Instance.new"Frame"
+local s=Instance.new"Frame"
+local t=Instance.new"Frame"
 
-local t=80
-local u=10
-local v=(t*4)+(u*3)
-local w=(f.AbsoluteSize.X-v)/2
+local u=80
+local v=10
+local w=(u*4)+(v*3)
+local x=(g.AbsoluteSize.X-w)/2
 
-createTab(g,UDim2.new(0,w,0,0),"Arsenal",k,p)
-createTab(h,UDim2.new(0,w+t+u,0,0),"Rivals",l,q)
-createTab(i,UDim2.new(0,w+2*(t+u),0,0),"Phantom Forces",m,r)
-createTab(j,UDim2.new(0,w+3*(t+u),0,0),"Universal",n,s)
-
-
-local x
-
+createTab(m,UDim2.new(0,x,0,0),"Arsenal",q,i)
+createTab(n,UDim2.new(0,x+u+v,0,0),"Rivals",r,j)
+createTab(o,UDim2.new(0,x+2*(u+v),0,0),"PF",s,k)
+createTab(p,UDim2.new(0,x+3*(u+v),0,0),"Universal",t,l)
 
 local function selectTab(y)
-for z,A in ipairs(o:GetChildren())do
-if A:IsA"Frame"and A.Name~=y then
+for z,A in ipairs(h:GetChildren())do
+if A:IsA"Frame"then
 A.Visible=false
 end
 end
 
-local B=o:FindFirstChild(y)
+local B=h:FindFirstChild(y)
 if B then
 B.Visible=true
 end
 
-
-for C,D in ipairs(f:GetChildren())do
+for C,D in ipairs(g:GetChildren())do
 if D:IsA"TextButton"then
 if D.Text==y then
 D.BackgroundColor3=Color3.fromRGB(80,80,80)
-x=D
 else
 D.BackgroundColor3=Color3.fromRGB(60,60,60)
 end
@@ -214,25 +200,30 @@ end
 end
 end
 
-
-g.MouseButton1Click:Connect(function()selectTab"Arsenal"end)
-h.MouseButton1Click:Connect(function()selectTab"Rivals"end)
-i.MouseButton1Click:Connect(function()selectTab"Phantom Forces"end)
-j.MouseButton1Click:Connect(function()selectTab"Universal"end)
-
+m.MouseButton1Click:Connect(function()selectTab"Arsenal"end)
+n.MouseButton1Click:Connect(function()selectTab"Rivals"end)
+o.MouseButton1Click:Connect(function()selectTab"PF"end)
+p.MouseButton1Click:Connect(function()selectTab"Universal"end)
 
 selectTab"Arsenal"
 
-
 local function toggleGuiVisibility(y)
 if y.KeyCode==Enum.KeyCode.RightShift then
-c.Enabled=not c.Enabled
+if d.Enabled then
+local A=c:Create(e,TweenInfo.new(0.5),{Position=UDim2.new(0.5,-225,1.5,0)})
+A:Play()
+A.Completed:Connect(function()
+d.Enabled=false
+end)
+else
+d.Enabled=true
+local A=c:Create(e,TweenInfo.new(0.5),{Position=UDim2.new(0.5,-225,0.5,-300)})
+A:Play()
 end
 end
-
+end
 
 a.InputBegan:Connect(toggleGuiVisibility)
 end
-
 
 createUI()
